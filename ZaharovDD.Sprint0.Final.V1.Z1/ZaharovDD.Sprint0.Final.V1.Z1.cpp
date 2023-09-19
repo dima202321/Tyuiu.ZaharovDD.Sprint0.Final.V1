2f53fe4c29014c9e55cc066bc867ba0dd3b803e0
Tyuiu.ZaharovDD <dimazaxarov.tyuiu@hotmail.com>
2023-09-19 18:05:49 +0500

    Add project files.

---
+++ ZaharovDD.Sprint0.Final.V1.Z1/ZaharovDD.Sprint0.Final.V1.Z1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+    cout << "Введите трехзначное число: ";
+    cin >> number;
+
+    if (number < 100 || number > 999) {
+        cout << "Ошибка: Введите трехзначное число." << endl;
+        return 1;
+    }
+
+    int digit1 = number % 10;
+    int digit2 = (number / 10) % 10;
+    int digit3 = number / 100;
+
+    int product = digit1 * digit2 * digit3;
+
+    cout << "Произведение цифр: " << product << endl;
+
+    return 0;
+}
---
+++ ZaharovDD.Sprint0.Final.V1.Z2/ZaharovDD.Sprint0.Final.V1.Z2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double x, y, z;
+    cout << "Введите значения переменных x, y и z через пробел: ";
+    cin >> x >> y >> z;
+
+    double result = 5 + 2 * x - z / 3 + pow(y, 2);
+
+    cout << "Значение выражения 5 + 2x - z/3 + y^2 равно: " << result << endl;
+
+    return 0;
+}
